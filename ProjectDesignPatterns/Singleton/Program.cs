c05335fd8be64d86b4c0fdd67bf7c5ff4d5d9de5
Renan Lucena <renansouto.lucena@live.com>
2020-07-21 23:44:24 +0100

    Singleton

---
+++ ProjectDesignPatterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton msg1 = Singleton.GetInstance;
+            msg1.Message("1st call - It's being instantiated, because it's the first time it's being called!");
+
+            Singleton msg2 = Singleton.GetInstance;
+            msg2.Message("2nd call - Not instantiated, because it was already done on the first call!");
+
+            Singleton msg3 = Singleton.GetInstance;
+            msg3.Message("3rd call - Not instantiated, because it was already done on the first call!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectDesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        //Create an object of the type Singleton 
+        private static Singleton conn = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (conn == null)
+                {
+                    conn = new Singleton();
+                    Console.WriteLine("******************* INSTANTIATED ************************");
+                }
+                return conn;
+
+            }
+        }
+
+        public void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
