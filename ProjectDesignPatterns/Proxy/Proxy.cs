c690ea62a39c67d23bb0d92c3d6976a73ea03c9c
Renan Lucena <renansouto.lucena@live.com>
2020-07-23 02:40:12 +0100

    AddProxy

---
+++ ProjectDesignPatterns/Proxy/Proxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Proxy : Subject
+    {
+        private RealSubject realSubject;
+        public override void Request()
+        {
+            if (this.realSubject==null)
+            {
+                this.realSubject = new RealSubject();
+            }
+            this.realSubject.Request();
+        }
+    }
+}
